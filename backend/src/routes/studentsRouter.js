4ec36371864e53e19545efffde533b26204708d7
AlekBalc <balc.aleksas@gmail.com>
2024-12-03 18:22:08 +0200

    define routes

--- backend/src/routes/studentsRouter.js
+++ backend/src/routes/studentsRouter.js
@@ -3,5 +3,9 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", getStudentsController);
+router.post("/", (req, res) => {});
+router.get("/:username", (req, res) => {});
+router.put("/:username", (req, res) => {});
+router.delete("/:username", (req, res) => {});
 
 export default router;
